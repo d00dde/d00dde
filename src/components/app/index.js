fd62b09b460e17cf1ee79775e61d50065b47f974
d00dde <d48564@gmail.com>
2020-05-25 22:43:42 +0300

    Update PDF and clean repo

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import withFetch from '../hoc/withFetch';
+import React from "react";
+import withFetch from "../hoc/withFetch";
 
-import {Navbar, Footer, Fab} from '../UI';
-import {HomePage, PortfolioPage, SummaryPage, ArticlePage, ArticlesPage } from '../pages';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Navbar, Footer, Fab } from "../UI";
+import {
+  HomePage,
+  PortfolioPage,
+  SummaryPage,
+  ArticlePage,
+  ArticlesPage
+} from "../pages";
+import Summary from "../modules/summary";
+import { Route, Switch, Redirect } from "react-router-dom";
 
- const App = (props) => {
+const App = props => {
   const { host, colors, contacts, portfolio, summary } = props.data;
   return (
-    <div className='container app'>
-      <Navbar data={{colors}}/>
+    <div className="container app">
+      <Navbar data={{ colors }} />
       <Switch>
-        <Route path='/' render={()=> <HomePage data={{host, colors}}/>} exact/>
-        <Route path='/summary' render={()=> <SummaryPage data={{summary, colors}}/>} />
-        <Route path='/portfolio' render={()=> <PortfolioPage data={{host, portfolio, colors}}/>} />
-        <Route path='/articles' render={()=> <ArticlesPage colors={colors}/>}/>        
-        <Route path='/article/:id' render={({ match })=> <ArticlePage colors={colors} match={match}/>} />
-        <Redirect to='/' />
+        <Route
+          path="/"
+          render={() => <HomePage data={{ host, colors }} />}
+          exact
+        />
+        <Route
+          path="/summary"
+          render={() => <SummaryPage data={{ summary, colors }} />}
+        />
+        <Route
+          path="/summary-ru"
+          render={() => <Summary data={{ summary, colors }} language={"rus"} />}
+        />
+        <Route
+          path="/summary-eng"
+          render={() => <Summary data={{ summary, colors }} language={"eng"} />}
+        />
+        <Route
+          path="/portfolio"
+          render={() => <PortfolioPage data={{ host, portfolio, colors }} />}
+        />
+        <Route
+          path="/articles"
+          render={() => <ArticlesPage colors={colors} />}
+        />
+        <Route
+          path="/article/:id"
+          render={({ match }) => <ArticlePage colors={colors} match={match} />}
+        />
+        <Redirect to="/" />
       </Switch>
-      <Footer data={{colors, contacts}}/>
-      <Fab data={{colors, contacts}}/>
+      <Footer data={{ colors, contacts }} />
+      <Fab data={{ colors, contacts }} />
     </div>
   );
-}
+};
 
-export default withFetch(App)('getMainData');
+export default withFetch(App)("getMainData");
--- src/components/modules/summary/index.js
+++ src/components/modules/summary/index.js
@@ -1,71 +1,99 @@
-import React from 'react';
-import './summary.css';
+import React from "react";
+import "./summary.css";
 
 export default ({ language, data }) => {
-  const {personal, about, skills, education, experience, additional } = data.summary[language];
+  console.log(data);
+  console.log(language);
+  const {
+    personal,
+    about,
+    skills,
+    education,
+    experience,
+    additional
+  } = data.summary[language];
   return (
-		<div className="summary-container">
+    <div className="summary-container">
       <div className="personal">
-        <div className="photo"><img src={personal.personImgSrc} alt="person" /></div>
+        <div className="photo">
+          <img src={personal.personImgSrc} alt="person" />
+        </div>
         <div className="personal-data">
           <h2 className="name">{personal.name}</h2>
           <h3 className="position">{personal.position}</h3>
           <div className="contacts">
-            {personal.contacts.map((contact) => {
-              return(<div key={contact.title}>
-                      <div className='icon'>
-                        <i className={contact.icon}></i>
-                      </div>
-                      <span>{contact.value}</span>
-                    </div>);
+            {personal.contacts.map(contact => {
+              return (
+                <div key={contact.title}>
+                  <div className="icon">
+                    <i className={contact.icon}></i>
+                  </div>
+                  <span>{contact.value}</span>
+                </div>
+              );
             })}
           </div>
         </div>
       </div>
       <div className="columns">
         <div className="left-col">
-          <h3><i className={about.icon}></i>{about.title}</h3>
+          <h3>
+            <i className={about.icon}></i>
+            {about.title}
+          </h3>
           <div className="info">{about.info}</div>
         </div>
         <div className="right-col">
-        <div className="skills">
-          <h3><i className={skills.icon}></i>{skills.title}</h3>
-          <ul>
-            {skills.values.map((skill) => (
-              <li key={skill}>{skill}</li>
-            ))}
-          </ul>
-        </div>
-        <div className="education">
-            <h3><i className={education.icon}></i>{education.title}</h3>
-          <ul>
-            {education.values.map((item) => (
-              <li key={item.description}>
-                <span className='time'>{item.time}</span>
-                <span className='description'>{item.description}</span>
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="experience">
-          <h3><i className={experience.icon}></i>{experience.title}</h3>
-          <ul>
-            {experience.values.map((work) => (
-              <li key={work.__html} dangerouslySetInnerHTML={work}></li>
-            ))}
-          <div className='footnote'>{experience.footnote}</div>
-          </ul>
-        </div>
-        <div className="additional">
-          <h3><i className={additional.icon}></i>{additional.title}</h3>
-          <ul>
-           {additional.values.map((item) => (
-             <li key={item}>{item}</li>
-           ))}
-          </ul>
-        </div>    
+          <div className="skills">
+            <h3>
+              <i className={skills.icon}></i>
+              {skills.title}
+            </h3>
+            <ul>
+              {skills.values.map(skill => (
+                <li key={skill}>{skill}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="education">
+            <h3>
+              <i className={education.icon}></i>
+              {education.title}
+            </h3>
+            <ul>
+              {education.values.map(item => (
+                <li key={item.description}>
+                  <span className="time">{item.time}</span>
+                  <span className="description">{item.description}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="experience">
+            <h3>
+              <i className={experience.icon}></i>
+              {experience.title}
+            </h3>
+            <ul>
+              {experience.values.map(work => (
+                <li key={work.__html} dangerouslySetInnerHTML={work}></li>
+              ))}
+              <div className="footnote">{experience.footnote}</div>
+            </ul>
+          </div>
+          <div className="additional">
+            <h3>
+              <i className={additional.icon}></i>
+              {additional.title}
+            </h3>
+            <ul>
+              {additional.values.map(item => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
     </div>
-	);
-}
+  );
+};
